aa8aeac2d002102cd49bbe5b96594f2941468fd6
Tamiris Coutinho <tami_coliveira@hotmail.com>
2024-08-12 13:48:06 -0300

    alterando o valor da variavel idade

--- decisoes.php
+++ decisoes.php
@@ -1,26 +1,26 @@
 <?php
 
-$idade = 34;
+$idade = 36;
 // $nome = 'Tamiris';
 
 echo "Entrada permitida apenas para pessoas acima de 18 anos." . PHP_EOL;
- 
+
 // if ($idade == 18 || $idade > 18) { // pode usar or, >=
 //   echo "Você tem $idade anos." . PHP_EOL;
 //    echo "Pode Entrar" . PHP_EOL;
 //}
 
 // if ($idade >= 18 && $nome == 'Tamiris') {  //and &&
-     //     echo "Você tem $idade anos." . PHP_EOL;
+//     echo "Você tem $idade anos." . PHP_EOL;
 //    echo "Pode Entrar" . PHP_EOL;
 //}
 
 if ($idade >= 18) { // pode usar or, >=
     echo "Você tem $idade anos." . PHP_EOL;
-     echo "Pode Entrar" . PHP_EOL;
- }else{
-     echo "Você só tem $idade anos. Você não pode entrar";
- }
+    echo "Pode Entrar" . PHP_EOL;
+} else {
+    echo "Você só tem $idade anos. Você não pode entrar";
+}
 
- echo PHP_EOL;
- echo "Adeus!";
+echo PHP_EOL;
+echo "Adeus!";
